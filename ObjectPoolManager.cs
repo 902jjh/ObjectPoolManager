5ae784e18d24504ebd70f49b2da13a86b6bd23a6
902jjh <166824844+902jjh@users.noreply.github.com>
2026-03-18 18:55:16 +0900

    Add files via upload

---
+++ ObjectPoolManager.cs
@@ -0,0 +1,286 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManager : MonoBehaviour
+{
+
+    /*public static ObjectPoolManager Instance;
+
+    private GameObject parent;
+    private GameObject poolobject;
+
+    private string Name;
+
+
+    public class ObjectPool
+    {
+        [Header("Parent Name")]
+        public string Name;
+        //public GameObject pool_parent { get; set; }
+
+        [System.Serializable]
+        public class Data
+        {
+            public GameObject obj;
+            public int count;
+        }
+
+        public Data[] data;
+    }
+
+    [System.Serializable]
+    public class Type : ObjectPool
+    {
+
+    }
+
+    public Type[] type;
+
+    public IObjectPool<GameObject> Pool;
+
+    public Dictionary<string, IObjectPool<GameObject>> _Pool = new Dictionary<string, IObjectPool<GameObject>>();
+
+    public Dictionary<string, GameObject[]> PoolDict = new Dictionary<string, GameObject[]>();
+
+    public GameObject eee;
+
+    // 부모, 대표 오브젝트
+    
+    private void Start()
+    {
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this.gameObject);
+
+        InitSetting();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+
+        }
+    }
+
+    private void InitSetting()
+    {
+        for (int i = 0; i < type.Length; i++)
+        {
+            GameObject parent = new GameObject(type[i].Name);
+            parent.transform.parent = this.transform;
+
+            for (int ii = 0; ii < type[i].data.Length; ii++)
+            {
+                GameObject child = new GameObject(type[i].data[ii].obj.name); // 풀링들의 부모가 될 오브젝트
+                child.transform.parent = parent.transform;
+
+                PoolDict.Add(child.name, new GameObject[2] { type[i].data[ii].obj, child });
+
+                _Pool.Add(type[i].data[ii].obj.name, new ObjectPool<GameObject>(CreatePoolObject,
+                    UsePoolObject, UnUsePoolObject, DestroyPoolObject, true, type[i].data[ii].count, type[i].data[ii].count));
+                
+                Name = type[i].data[ii].obj.name.ToString();
+
+                this.poolobject = type[i].data[ii].obj;
+                this.parent = child;
+
+                for (int iii = 0; iii < type[i].data[ii].count; iii++)
+                {
+                    GameObject pool = CreatePoolObject();
+                    _Pool[type[i].data[ii].obj.name].Release(pool);
+                }
+            }
+        }
+    }
+
+
+    private GameObject CreatePoolObject()
+    {
+        GameObject pool = Instantiate(this.poolobject, this.parent.transform);
+        if(pool.GetComponent<IPoolable>() != null) pool.GetComponent<IPoolable>().Pool = _Pool[poolobject.name];
+        return pool;
+    }
+
+    private void UsePoolObject(GameObject pool)
+    {
+        pool.SetActive(true);
+        OverTimeReleasePool(pool);
+    }
+
+    private void UnUsePoolObject(GameObject pool)
+    {
+        pool.SetActive(false);
+    }
+
+    private void DestroyPoolObject(GameObject pool)
+    {
+        Destroy(pool);
+    }
+
+    public void GetGo(string name)
+    {
+        _Pool[name].Get();
+        Name = name;
+        //parent = PoolDict[name][1];
+        //poolobject = PoolDict[name][0];
+        
+    }
+
+    public void ReleasePool()
+    {
+
+    }
+
+    public void OverTimeReleasePool(GameObject pool)
+    {
+
+        StartCoroutine(releasePool());
+
+        IEnumerator releasePool()
+        {
+            yield return new WaitForSecondsRealtime(5);
+            if(pool.activeSelf) _Pool[pool.transform.parent.name].Release(pool);
+        }
+    }*/
+
+    public static ObjectPoolManager Instance;
+
+    private Dictionary<string, GameObject> PoolDict = new Dictionary<string, GameObject>();
+    private Dictionary<string, GameObject> PoolParent = new Dictionary<string, GameObject>();
+    private Dictionary<string, int> PoolCount = new Dictionary<string, int>();
+
+    [System.Serializable]
+    public class ObjectPool
+    {
+        [Header("Parent Name")]
+        public string Name;
+
+        [System.Serializable]
+        public class Data
+        {
+            public GameObject obj;
+            public int count;
+        }
+
+        public Data[] data;
+    }
+
+    [SerializeField] private ObjectPool[] objectpool;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this.gameObject);
+
+        InitSetting();
+    }
+
+    private void InitSetting()
+    {
+        for (int i = 0; i < objectpool.Length; i++)
+        {
+            ObjectPool pools = objectpool[i];
+            for (int ii = 0; ii < pools.data.Length; ii++)
+            {
+                GameObject parent = new GameObject(pools.data[ii].obj.name);
+                GameObject child = pools.data[ii].obj;
+
+                parent.transform.parent = this.transform;
+                
+                PoolDict.Add(parent.name, pools.data[ii].obj);
+                PoolParent.Add(parent.name, parent);
+                PoolCount.Add(parent.name, pools.data[ii].count);
+
+                int count = pools.data[ii].count;
+                
+                for (int iii = 0; iii < count; iii++)
+                {
+                    GameObject pool = SpawnObjectPool(parent);
+                    pool.SetActive(false);
+                    //UnUseObjectPool(pool);
+                    // 위에껀 혹시 모르니까 남겨둠 나중에 Bullet한테 맞춰줬던게 문제될 수 있으니까
+                    // Bullet의 OnDeSpawn GameObject effect관련
+                }
+            }
+        }
+    }
+
+    private GameObject SpawnObjectPool(GameObject pool)
+    {
+        string name = pool.name;
+        GameObject obj = Instantiate(PoolDict[name], PoolParent[name].transform);
+        return obj;
+    }
+
+    private void DeSpawnObjectPool(GameObject pool)
+    {
+        Destroy(pool.transform.gameObject);
+    }
+
+    private void UseObjectPool(GameObject pool)
+    {
+        pool.SetActive(true);
+        pool.GetComponent<IPoolable>()?.OnSpawn();
+    }
+
+    private void UnUseObjectPool(GameObject pool)
+    {
+        pool.SetActive(false);
+        pool.GetComponent<IPoolable>()?.OnDeSpawn();
+    }
+
+    public GameObject Get(GameObject pool)
+    {
+        // 새로 만들지 아니면 기존에 있던것을 재활용할지
+        //int count = PoolCount[pool.name];
+        GameObject parent = PoolParent[pool.name];
+
+        GameObject obj = null;
+        bool IsCreate = true;
+
+        for (int i = 0; i < parent.transform.childCount; i++)
+        {
+            GameObject item = parent.transform.GetChild(i).gameObject;
+            if (item.gameObject.activeSelf == false) // 생성 말고 재활용 가능하면
+            {
+                IsCreate = false;
+                obj = item.gameObject;
+                break;
+            }
+        }
+
+        if (IsCreate)
+        {
+            obj = SpawnObjectPool(pool);
+            obj.GetComponent<IPoolable>()?.OnSpawn();
+        }
+        else
+            UseObjectPool(obj);
+
+        return obj;
+
+    }
+
+    public void Release(GameObject pool)
+    {
+        string obj = PoolDict[pool.transform.parent.name]?.name;
+        int defaultcount = PoolCount[obj];
+        int childcount = PoolParent[obj].transform.childCount;
+
+        if (defaultcount >= childcount)
+        {
+            UnUseObjectPool(pool);
+        }else
+        {
+            DeSpawnObjectPool(pool);
+        }
+    }
+}
