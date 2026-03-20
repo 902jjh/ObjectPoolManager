5651eaa60cfedb685f030e32524ce1f7da9133c1
902jjh <166824844+902jjh@users.noreply.github.com>
2026-03-20 09:57:46 +0900

    Update ObjectPoolManager.cs

--- ObjectPoolManager.cs
+++ ObjectPoolManager.cs
@@ -3,154 +3,7 @@ using UnityEngine;
 
 public class ObjectPoolManager : MonoBehaviour
 {
-
-    /*public static ObjectPoolManager Instance;
-
-    private GameObject parent;
-    private GameObject poolobject;
-
-    private string Name;
-
-
-    public class ObjectPool
-    {
-        [Header("Parent Name")]
-        public string Name;
-        //public GameObject pool_parent { get; set; }
-
-        [System.Serializable]
-        public class Data
-        {
-            public GameObject obj;
-            public int count;
-        }
-
-        public Data[] data;
-    }
-
-    [System.Serializable]
-    public class Type : ObjectPool
-    {
-
-    }
-
-    public Type[] type;
-
-    public IObjectPool<GameObject> Pool;
-
-    public Dictionary<string, IObjectPool<GameObject>> _Pool = new Dictionary<string, IObjectPool<GameObject>>();
-
-    public Dictionary<string, GameObject[]> PoolDict = new Dictionary<string, GameObject[]>();
-
-    public GameObject eee;
-
-    // ºÎ¸ğ, ´ëÇ¥ ¿ÀºêÁ§Æ®
-    
-    private void Start()
-    {
-    }
-
-    private void Awake()
-    {
-        if (Instance == null)
-            Instance = this;
-        else
-            Destroy(this.gameObject);
-
-        InitSetting();
-    }
-
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.K))
-        {
-
-        }
-    }
-
-    private void InitSetting()
-    {
-        for (int i = 0; i < type.Length; i++)
-        {
-            GameObject parent = new GameObject(type[i].Name);
-            parent.transform.parent = this.transform;
-
-            for (int ii = 0; ii < type[i].data.Length; ii++)
-            {
-                GameObject child = new GameObject(type[i].data[ii].obj.name); // Ç®¸µµéÀÇ ºÎ¸ğ°¡ µÉ ¿ÀºêÁ§Æ®
-                child.transform.parent = parent.transform;
-
-                PoolDict.Add(child.name, new GameObject[2] { type[i].data[ii].obj, child });
-
-                _Pool.Add(type[i].data[ii].obj.name, new ObjectPool<GameObject>(CreatePoolObject,
-                    UsePoolObject, UnUsePoolObject, DestroyPoolObject, true, type[i].data[ii].count, type[i].data[ii].count));
-                
-                Name = type[i].data[ii].obj.name.ToString();
-
-                this.poolobject = type[i].data[ii].obj;
-                this.parent = child;
-
-                for (int iii = 0; iii < type[i].data[ii].count; iii++)
-                {
-                    GameObject pool = CreatePoolObject();
-                    _Pool[type[i].data[ii].obj.name].Release(pool);
-                }
-            }
-        }
-    }
-
-
-    private GameObject CreatePoolObject()
-    {
-        GameObject pool = Instantiate(this.poolobject, this.parent.transform);
-        if(pool.GetComponent<IPoolable>() != null) pool.GetComponent<IPoolable>().Pool = _Pool[poolobject.name];
-        return pool;
-    }
-
-    private void UsePoolObject(GameObject pool)
-    {
-        pool.SetActive(true);
-        OverTimeReleasePool(pool);
-    }
-
-    private void UnUsePoolObject(GameObject pool)
-    {
-        pool.SetActive(false);
-    }
-
-    private void DestroyPoolObject(GameObject pool)
-    {
-        Destroy(pool);
-    }
-
-    public void GetGo(string name)
-    {
-        _Pool[name].Get();
-        Name = name;
-        //parent = PoolDict[name][1];
-        //poolobject = PoolDict[name][0];
-        
-    }
-
-    public void ReleasePool()
-    {
-
-    }
-
-    public void OverTimeReleasePool(GameObject pool)
-    {
-
-        StartCoroutine(releasePool());
-
-        IEnumerator releasePool()
-        {
-            yield return new WaitForSecondsRealtime(5);
-            if(pool.activeSelf) _Pool[pool.transform.parent.name].Release(pool);
-        }
-    }*/
-
     public static ObjectPoolManager Instance;
-
     private Dictionary<string, GameObject> PoolDict = new Dictionary<string, GameObject>();
     private Dictionary<string, GameObject> PoolParent = new Dictionary<string, GameObject>();
     private Dictionary<string, int> PoolCount = new Dictionary<string, int>();
@@ -206,8 +59,8 @@ public class ObjectPoolManager : MonoBehaviour
                     GameObject pool = SpawnObjectPool(parent);
                     pool.SetActive(false);
                     //UnUseObjectPool(pool);
-                    // À§¿¡²« È¤½Ã ¸ğ¸£´Ï±î ³²°ÜµÒ ³ªÁß¿¡ BulletÇÑÅ× ¸ÂÃçÁá´ø°Ô ¹®Á¦µÉ ¼ö ÀÖÀ¸´Ï±î
-                    // BulletÀÇ OnDeSpawn GameObject effect°ü·Ã
+                    // ìœ„ì—ê»€ í˜¹ì‹œ ëª¨ë¥´ë‹ˆê¹Œ ë‚¨ê²¨ë‘  ë‚˜ì¤‘ì— Bulletí•œí…Œ ë§ì¶°ì¤¬ë˜ê²Œ ë¬¸ì œë  ìˆ˜ ìˆìœ¼ë‹ˆê¹Œ
+                    // Bulletì˜ OnDeSpawn GameObject effectê´€ë ¨
                 }
             }
         }
@@ -239,7 +92,7 @@ public class ObjectPoolManager : MonoBehaviour
 
     public GameObject Get(GameObject pool)
     {
-        // »õ·Î ¸¸µéÁö ¾Æ´Ï¸é ±âÁ¸¿¡ ÀÖ´ø°ÍÀ» ÀçÈ°¿ëÇÒÁö
+        // ìƒˆë¡œ ë§Œë“¤ì§€ ì•„ë‹ˆë©´ ê¸°ì¡´ì— ìˆë˜ê²ƒì„ ì¬í™œìš©í• ì§€
         //int count = PoolCount[pool.name];
         GameObject parent = PoolParent[pool.name];
 
@@ -249,7 +102,7 @@ public class ObjectPoolManager : MonoBehaviour
         for (int i = 0; i < parent.transform.childCount; i++)
         {
             GameObject item = parent.transform.GetChild(i).gameObject;
-            if (item.gameObject.activeSelf == false) // »ı¼º ¸»°í ÀçÈ°¿ë °¡´ÉÇÏ¸é
+            if (item.gameObject.activeSelf == false) // ìƒì„± ë§ê³  ì¬í™œìš© ê°€ëŠ¥í•˜ë©´
             {
                 IsCreate = false;
                 obj = item.gameObject;
